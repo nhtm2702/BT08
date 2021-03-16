94e42f60fb702877e3f5f8958e83052ec9dc6ad4
nhtm2702 <78303807+nhtm2702@users.noreply.github.com>
2021-03-17 02:22:15 +0700

    first

---
+++ A2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+/* kich thuoc char la 1
+int main( )
+{
+   char a[4] = "abc";
+   for (char *cp = a; (*cp) != '\0'; cp++) {
+      cout << (void*) cp << " : " << (*cp) << endl;
+   }
+   return 0;
+}*/
+/*kich thuoc int la 4
+int main( )
+{
+    int a=0;
+    int *cp = &a;
+    cout << cp << endl;
+    cp ++ ;
+    cout << cp << endl;
+    return 0;
+}*/
+//Du lieu double tuong tu voi int
+/*Thay cp++ thanh cp+=2
+int main( )
+{
+    int a=0;
+    int *cp = &a;
+    cout << cp << endl;
+    cp+=2;
+    cout << cp << endl;
+    return 0;
+}*/
